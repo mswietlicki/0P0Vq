37071f361e862fa2e53bb555792e7a78f562ebc0
Mateusz Świetlicki <mateusz@swietlicki.net>
2014-01-17 11:52:54 +0100

    Added projects and first test example definition

---
+++ 0P0Vq.Tests/GivenExamplesTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace _0P0Vq.Tests
+{
+    public class GivenExamplesTests
+    {
+
+        [Fact]
+        public void Example1()
+        {
+            var n = 3;
+            var connections = new[] { new[] { 0, 1 }, new[] { 1, 2 }, new[] { 0, 2 } };
+            var k = 2;
+            var startIndexes = new[] { 0, 1 };
+
+
+        }
+    }
+}
---
+++ 0P0Vq/ExpenseCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0P0Vq
+{
+    public class ExpenseCalculator
+    {
+        public ExpenseCalculator()
+        {
+            
+        }
+
+        public IEnumerable<Expense> CalculateExpenses()
+        {
+            
+
+        }
+    }
+
+    public class Expense
+    {
+    }
+}
