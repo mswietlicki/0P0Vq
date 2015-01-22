2ce534a83de239f7dccd5714841cda30b9f8c5f0
Mateusz Świetlicki <mateusz@swietlicki.net>
2015-01-22 18:25:13 +0100

    ExpenseCalculator and Tests

--- 0P0Vq.Tests/GivenExamplesTests.cs
+++ 0P0Vq.Tests/GivenExamplesTests.cs
@@ -10,20 +10,78 @@ namespace _0P0Vq.Tests
         [Fact]
         public void Example1()
         {
-            var n = 3;
-            var connections = new[] { new[] { 0, 1 }, new[] { 1, 2 }, new[] { 0, 2 } };
-            var k = 2;
-            var startIndexes = new[] { 0, 1 };
+            var nodes = new[]
+            {
+                new Node {Id = 0},
+                new Node {Id = 1},
+                new Node {Id = 2},
+            };
+
+            nodes[0].AddConnection(nodes[1]);
+            nodes[1].AddConnection(nodes[2]);
+            nodes[0].AddConnection(nodes[2]);
+
+            var candidates = new[]
+            {
+                new Candidate { Id = 0, StartNode = nodes[0]},
+                new Candidate { Id = 1, StartNode = nodes[1]}
+            };
 
             var container = new Container();
 
             var calulator = container.GetInstance<ExpenseCalculator>() as IExpenseCalculator;
 
-            var expenses = calulator.CalculateExpenses(n, connections, k, startIndexes).ToArray();
+            var expenses = calulator.CalculateExpenses(nodes, candidates).ToArray();
 
             Assert.NotEmpty(expenses);
             Assert.Equal(expenses[0], 1);
             Assert.Equal(expenses[1], 2);
         }
+
+        [Fact]
+        public void Example2()
+        {
+            var nodes = new[]
+            {
+                new Node {Id = 0},
+                new Node {Id = 1},
+                new Node {Id = 2},
+                new Node {Id = 3},
+                new Node {Id = 4},
+                new Node {Id = 5},
+            };
+
+            nodes[1].AddConnection(nodes[0]);
+            nodes[2].AddConnection(nodes[1]);
+            nodes[3].AddConnection(nodes[0]);
+            nodes[3].AddConnection(nodes[1]);
+            nodes[3].AddConnection(nodes[2]);
+            nodes[4].AddConnection(nodes[2]);
+            nodes[4].AddConnection(nodes[3]);
+            nodes[5].AddConnection(nodes[0]);
+            nodes[5].AddConnection(nodes[2]);
+
+            var candidates = new[]
+            {
+                new Candidate { Id = 0, StartNode = nodes[0]},
+                new Candidate { Id = 1, StartNode = nodes[1]},
+                new Candidate { Id = 2, StartNode = nodes[5]},
+            };
+
+            var container = new Container();
+
+            var calulator = container.GetInstance<ExpenseCalculator>() as IExpenseCalculator;
+
+            var expenses = calulator.CalculateExpenses(nodes, candidates).ToArray();
+
+            Assert.NotEmpty(expenses);
+            Assert.Equal(1, expenses[0]);
+            Assert.Equal(2, expenses[1]);
+            Assert.Equal(3, expenses[2]);
+        }
+
+
+
+
     }
 }
--- 0P0Vq/ExpenseCalculator.cs
+++ 0P0Vq/ExpenseCalculator.cs
@@ -1,33 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _0P0Vq
 {
     public interface IExpenseCalculator
     {
-        IEnumerable<int> CalculateExpenses();
-        IEnumerable<int> CalculateExpenses(int n, int[][] connections, int k, int[] startIndexes);
+        IEnumerable<int> CalculateExpenses(IEnumerable<Node> nodes, IEnumerable<Candidate> candidates);
     }
 
     public class ExpenseCalculator : IExpenseCalculator
     {
-        public ExpenseCalculator()
-        {
 
+        public IEnumerable<int> CalculateExpenses(IEnumerable<Node> nodes, IEnumerable<Candidate> candidates)
+        {
+            return CalculateExpenses(nodes.ToList(), candidates.ToList());
         }
-
-        public IEnumerable<int> CalculateExpenses()
+        public IEnumerable<int> CalculateExpenses(List<Node> nodes, List<Candidate> candidates)
         {
-            return null;
-        }
+            bool somethingChanged = true;
+            while (somethingChanged)
+            {
+                somethingChanged = false;
+                foreach (var candidate in candidates)
+                {
+                    foreach (var node in nodes.Where(n => n.HQ == candidate).SelectMany(n => n.Connections))
+                    {
+                        if (node.HQ == null)
+                            node.Committes.Add(candidate);
+                    }
+                }
 
-        public IEnumerable<int> CalculateExpenses(int n, int[][] connections, int k, int[] startIndexes)
-        {
-            return null;
+                foreach (var node in nodes.Where(n => n.Committes.Any()))
+                {
+                    var winner =
+                        node.Connections.Where(n => n.HQ != null).GroupBy(n => n.HQ)
+                            .Select(g => new { Cantidate = g.Key, Count = g.Count() })
+                            .OrderByDescending(g => g.Count)
+                            .First()
+                            .Cantidate;
+                    if (node.HQ != winner)
+                        somethingChanged = true;
+
+                    node.HQ = winner;
+                    node.Committes.Remove(winner);
+                }
+            }
+            return candidates.Select(c => nodes.Count(n => n.HQ == c));
         }
     }
 
+    public class Node
+    {
+        public Node()
+        {
+            Connections = new List<Node>();
+            Committes = new List<Candidate>();
+        }
+        public int Id { get; set; }
+        public List<Node> Connections { get; set; }
+        public List<Candidate> Committes { get; set; }
+
+        public Candidate HQ { get; set; }
+
+        public void AddConnection(Node node)
+        {
+            this.Connections.Add(node);
+            node.Connections.Add(this);
+        }
+
+        public override string ToString()
+        {
+            return "Node: " + Id;
+        }
+    }
+
+    public class Candidate
+    {
+        public int Id { get; set; }
+
+        private Node _startNode;
+
+        public Node StartNode
+        {
+            get { return _startNode; }
+            set
+            {
+                _startNode = value;
+                _startNode.HQ = this;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Candidate: " + Id;
+        }
+    }
 }
