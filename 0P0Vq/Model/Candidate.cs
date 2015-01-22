0e661c81e44dd921c9a4ad7988e4527c41b8ff21
Mateusz Świetlicki <mateusz@swietlicki.net>
2015-01-22 18:32:58 +0100

    Move models to Model directory

---
+++ 0P0Vq/Model/Candidate.cs
@@ -0,0 +1,24 @@
+﻿namespace _0P0Vq
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+
+        private Node _startNode;
+
+        public Node StartNode
+        {
+            get { return _startNode; }
+            set
+            {
+                _startNode = value;
+                _startNode.HQ = this;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Candidate: " + Id;
+        }
+    }
+}
